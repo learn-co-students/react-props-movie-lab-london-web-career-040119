3bf16fac61cdc8c7b5d8cb8efb7aa147b7e61040
github username <github email address>
2019-06-03 17:42:27 +0100

    Done.

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -11,18 +11,28 @@ const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 export default class CardBack extends Component {
 
   generateRatingElement = () => {
-    // implement meeeee! See the readme for instructions
+    const element = this.props.IMDBRating === null ? <h4>No rating found</h4> : <img src={imgMapper[this.props.IMDBRating]} alt=""/>
+    return element
   }
 
+
   render() {
+    // console.log("props:", this.props)
     return (
       <div className="card-back">
-        <h3 className="title"></h3>
+        <h3 className="title">{this.props.title}</h3>
         <span />
-        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        {this.generateRatingElement()}
         <span />
-        <h5 className="genres"></h5>
+        <h5 className="genres">{this.props.genres.join(", ")}</h5>
       </div>
     )
   }
 }
+
+// CardBack.defaultProps = {
+//   title: 'Unknown',
+//   IMDBRating: null,
+//   genres: ['No Genre(s) Found'],
+//   poster: 'default'
+// }
